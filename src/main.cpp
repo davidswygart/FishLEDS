3f52fbbe06ebd7cc84e358a9ec7b17e57c0be5ab
David Swygart <davidswygart@gmail.com>
2024-09-04 23:07:52 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,22 @@ Ble ble;
 // Color Target LEDs
 const int RGBW_pins[4] = {13,27,25,32};//R/G/B/W
 
+int currentColor[4] = {200,0,0,180};
+int savedColors[5][4] = {
+  {0,0,0,0},
+  {200,0,0,180},
+  {151,161,161,255},
+  {200,0,0,180},
+  {0,0,0,0},
+  };
+// 7 AM -> 25,200
+// 9 AM -> 32,400
+// 2 PM -> 50,400
+// 9 PM -> 75,600
+// 10 PM -> 79,200
+// midnight -> 86,400
+unsigned long currentTimeSeconds = 33000;
+unsigned long savedTimes[5] = {25200, 32400, 50400, 75600, 79200};//{25200, 32400, 50400, 75600, 79200};
 
 
 void setupLed(int pin, int channel){
@@ -25,45 +41,49 @@ void setup() {
   NimBLEDevice::getAdvertising()->start(0);
 }
 
+int lastUpdateTimeSeconds = 0;
+void updateTime(){
+  unsigned long now = millis()/1000;
+  currentTimeSeconds += now - lastUpdateTimeSeconds; 
+  lastUpdateTimeSeconds = now;
 
-long int timeInSeconds = 78060;
-// 86400 seconds in a day
-// 9 AM -> 32400
-// 9 PM -> 75600
+  if (currentTimeSeconds > 86400){
+    currentTimeSeconds = currentTimeSeconds - 86400;
+  }
+}
+
+
+int nextTimeInd(){
+  int nextTime = 0;
+  for (int i=0; i<5; i++){
+    if(currentTimeSeconds<savedTimes[i]){
+      nextTime=i;
+      break;
+    }
+  }
+  return nextTime;
+}
+
+void updateColor(int nextColorInd){
+  int timeDif = savedTimes[nextColorInd] - currentTimeSeconds;
+
+  for (int ledInd=0; ledInd<4; ledInd++){
+    int futureVal = savedColors[nextColorInd][ledInd];
+    int currentVal = currentColor[ledInd];
+    int colorDif = futureVal - currentVal;
+    int change = colorDif / timeDif;
+    currentColor[ledInd] = currentVal + change;
+    ledcWrite(ledInd, currentColor[ledInd]);
+    Serial.print(currentColor[ledInd]); Serial.print(" , ");
+    
+  }
+  Serial.println();
+  
+}
 
 void loop() {
-
-  while(timeInSeconds<32400){
-    delay(1000);
-    timeInSeconds++;
-  }
-
-
-  for (int iSec=1; iSec<256; iSec++){
-    for(int iLED=0; iLED<4; iLED++){
-        ledcWrite(iLED, iSec);
-    }
-    delay(1000);
-    timeInSeconds++;
-  }
-
-  while(timeInSeconds<75600){
-    delay(1000);
-    timeInSeconds++;
-  }
-
-  for (int iSec=255; iSec>-1; iSec--){
-    for(int iLED=0; iLED<4; iLED++){
-        ledcWrite(iLED, iSec);
-    }
-    delay(1000);
-    timeInSeconds++;
-  }
-
-  while(timeInSeconds<86400){
-    delay(1000);
-    timeInSeconds++;
-  }
-
-  timeInSeconds = 0;
+  updateTime();
+  int ind = nextTimeInd();
+  updateColor(ind);
+  delay(1000);
 }
